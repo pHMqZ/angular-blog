423e26af40b58ccfd6dceca0caa7af782da83221
Phillip Marques <phillipmarq@hotmail.com>
2024-02-14 12:45:21 -0300

    Adjusting Menu title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MenuTitleComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
