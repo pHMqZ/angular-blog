03eccb1ea28b6533815da2843af1d50156de28f6
Phillip Marques <phillipmarq@hotmail.com>
2024-02-14 13:49:37 -0300

    Creating Big card structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, MenuTitleComponent]
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [MenuTitleComponent, BigCardComponent]
+})
+export class HomeComponent {
+
+}
